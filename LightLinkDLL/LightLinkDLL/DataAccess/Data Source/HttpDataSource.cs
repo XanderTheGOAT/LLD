870df4b1bc6f29b2955b0abe35129269ed598112
Alexander Quintanilla <32335891+XanderTheGOAT@users.noreply.github.com>
2019-05-31 00:24:08 -0600

    Finished Http Service

--- LightLinkDLL/LightLinkDLL/DataAccess/Data Source/HttpDataSource.cs
+++ LightLinkDLL/LightLinkDLL/DataAccess/Data Source/HttpDataSource.cs
@@ -16,9 +16,8 @@ namespace LightLinkDLL.DataAccess
 
         public string BaseUrl { get; }
         public string Username { get; }
-        private string responseString = null;
-        private bool finished = false;
         private Profile currentProfile;
+
 
         public HttpDataSource(string host, string username)
         {
@@ -26,38 +25,30 @@ namespace LightLinkDLL.DataAccess
             Username = username;
         }
 
-        private async void GetInformation(String url)
-        {
-            responseString = await client.GetStringAsync(url);
-            finished = true;
-        }
 
         public Profile GetProfile()
         {
-            if (currentProfile is null)
-            {
-                string profileRoute = BaseUrl + "/Profile/active/" + Username;
-                GetInformation(profileRoute);
-                if (finished)
-                {
-                    if (string.IsNullOrEmpty(responseString)) throw new Exception();
-                    JToken profileToken = JObject.Parse(responseString.Trim());
-                    finished = false;
-                    currentProfile = JsonConvert.DeserializeObject<Profile>(profileToken.ToString());
-                }
-            }
+            string profileRoute = BaseUrl + "/Profile/active/" + Username;
+            var task = client.GetStringAsync(profileRoute);
+            task.Wait();
+            var result = task.Result;
+            JToken profileToken = JObject.Parse(result);
+            currentProfile = JsonConvert.DeserializeObject<Profile>(profileToken.ToString());
             return currentProfile;
         }
 
-        public async void UpdateData(Computer computer)
+        public void UpdateData(Computer computer)
         {
             string profileRoute = BaseUrl + "/Computer";
             String serializedComputer = JsonConvert.SerializeObject(computer);
             Console.WriteLine(serializedComputer);
             StringContent computerQuery = new StringContent(serializedComputer);
-            computerQuery.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");               
-            HttpResponseMessage response = await client.PostAsync(profileRoute, computerQuery);
+            computerQuery.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+            var task = client.PostAsync(profileRoute, computerQuery);
+            task.Wait();
+            HttpResponseMessage response = task.Result;
             response.EnsureSuccessStatusCode();
+            Console.WriteLine(response.IsSuccessStatusCode);
         }
     }
 }
